9f6bd3297d6638e744ffc318349d01723622240a
Edvard Englund <edvard.englund@medsci.uu.se>
2020-09-24 10:48:19 +0200

    Script for pytest

--- bin_work/pytest.sh
+++ bin_work/pytest.sh
@@ -3,20 +3,6 @@
 
 command="pytest --no-print-logs --log-level=\"WARN\""
 
-while getopts "m:" opt; do
-    case $opt in
-	m)
-	    echo "found otional arg"
-	    command=$command" -m ${OPTARG}"
-	    ;;
-	\?)
-	    echo "Unknown optional argument"
-	    exit 1
-    esac
-done
-
-command=$command" $1"
-
-echo "$command"
+command=$command" $@"
 
 eval "$command"
