248cc198832f5375ee6af141aa059cc41956603a
Edvard Englund <edvard.englund@medsci.uu.se>
2020-10-15 10:18:24 +0200

    Free text search in git-search-pull-reqeusts

--- bin_work/getopts_example.sh
+++ bin_work/getopts_example.sh
@@ -1,18 +1,38 @@
 #! /bin/bash
 # Usage: -c <parent commit or branch> -u <username>
+echo "starting"
+declare -i arg_counter=$#
+
 while getopts "c:u:" opt; do
 	case $opt in
 		c)
-			commit="${OPTARG}"
-			;;
+		    commit="${OPTARG}"
+		    echo "captured option: $commit"
+		    ;;
 		u)
-			user="${OPTARG}"
-			;;
+		    user="${OPTARG}"
+		    echo "captioned option: $user"
+		    ;;
 		\?)
-			exit 1
-			;;
+		    echo "Unknown option: ${OPTARG}"
+		    exit 1
+		    ;;
 	esac
+	arg_counter=$arg_counter-2
+	shift 2
 done
+
+
+all_text="a long long string"
+
+echo "Argument #1 is: $1"
+echo "Argument #2 is: $2"
+echo "Number arguments from counter: $arg_counter"
+echo "Number of arguments from built in: $#"
 echo "commit is $commit"
+if [[ $"$all_text" == *"$commit"* && "1" == "1" ]]; then
+    echo "found you!"
+fi
+
 echo "user is $user"
 
--- bin_work/git-search-pull-reqeuests.sh
+++ bin_work/git-search-pull-reqeuests.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 # Usage: <parent commit or branch> <username>
+
+while getopts "s:" opt; do
+	case $opt in
+		s)
+		    free_search="${OPTARG}"
+		    ;;
+		\?)
+		    echo "Unknown option: ${OPTARG}"
+		    exit 1
+		    ;;
+	esac
+	shift 2
+done
+
+
 if [[ $# -ne 2 ]]; then
-	echo "Usage: <parent commit> <git user name>"
+	echo "Usage: [-s free search] <parent commit> <git user name>"
 	exit 1
 fi
 
@@ -9,7 +24,11 @@ git rev-list --merges "$1" |
     while read rev; do
         p2=$(git rev-parse ${rev}^2)
         person=$(git log --no-walk --pretty=format:%cn $p2)
-        [ "$person" == "$2" ] && echo $rev
+	all_text=$(git log --no-walk ${rev})
+	if [[ "$person" == "$2" && $all_text == *"$free_search"* ]]; then
+	    echo $rev
+	fi
+	
     done |
     git log --stdin --no-walk # add formatting etc as desired
 
