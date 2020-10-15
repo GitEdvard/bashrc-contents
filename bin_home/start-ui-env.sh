1221da70b0df4ea07c2a059dd18bbe082afbdd31
Edvard Englund <edvard.englund@medsci.uu.se>
2020-10-15 10:16:20 +0200

    fix start-ui-env

--- bin_home/start-ui-env.sh
+++ bin_home/start-ui-env.sh
@@ -1,9 +1,9 @@
-google-chrome &
-drawio &
-slack & 
-code . &
+google-chrome > /dev/null 2>&1 & 
+drawio > /dev/null 2>&1 &
+slack > /dev/null 2>&1 & 
 cd $HOME/sources/commonlims
 echo "(start-all.sh) cd to ${HOME}/sources/commonlims"
+code . > /dev/null 2>&1 &
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 conda activate clims2
