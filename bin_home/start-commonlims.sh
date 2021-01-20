082d762efcbabcf807bd8b03e9082d982d750633
Edvard Englund <edvard.englund@medsci.uu.se>
2021-01-20 08:34:06 +0100

    Changes in start delayed commonlims

--- bin_home/start-commonlims.sh
+++ bin_home/start-commonlims.sh
@@ -1,8 +1,9 @@
 echo "(start-commonlims.sh) cd to /home/edvard/sources/commonlims"
 cd /home/edvard/sources/commonlims
 #CONDA_BASE=$(conda info --base)
-CONDA_BASE=/home/edvard/Installations/miniconda3
-source $CONDA_BASE/etc/profile.d/conda.sh
-conda activate clims2
+eval "$(/home/edvard/Installations/miniconda3/bin/conda shell.bash hook)" # fix for source activate within a subshell...
+#CONDA_BASE=/home/edvard/Installations/miniconda3
+#source $CONDA_BASE/etc/profile.d/conda.sh
+source activate clims2
 echo "(start-commonlims.sh) running make fresh"
 make fresh && pytest tests/clims
