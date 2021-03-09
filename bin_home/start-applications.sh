951566d779b71f09de63b89ffe1f1eb6867990b6
Edvard Englund <edvard.englund@medsci.uu.se>
2021-03-09 11:15:11 +0100

    Separate export file for home

--- bin_home/start-applications.sh
+++ bin_home/start-applications.sh
@@ -4,4 +4,4 @@ date
 google-chrome > /dev/null 2>&1 &
 drawio > /dev/null 2>&1 &
 slack > /dev/null 2>&1 & 
-#/opt/pycharm-2020.2.3/bin/pycharm.sh > /dev/null 2>&1 &
+/opt/pycharm-2020.2.3/bin/pycharm.sh > /dev/null 2>&1 &
