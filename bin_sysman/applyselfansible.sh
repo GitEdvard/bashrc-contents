fe7159c4e7a59efb0d5486bdf896ef5ed4cda400
Edvard Englund <edvard.englund@medsci.uu.se>
2021-12-30 10:26:57 +0100

    Apply self ansible

---
+++ bin_sysman/applyselfansible.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# Apply ansible playbook to the current computer. sudo must be activated in order for it to work.
+
+if [[ $# -eq 0 ]]; then
+	echo "Usage: play.sh [options] <path to playbook>"
+	exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+ansible-playbook -i /home/edvard/sources/admin/ansible/inventories/dev_inventory "$@"
+
+
