78ee487a61f86f02f0208eec7ef684e5d29ee24e
Edvard Englund <edvard.englund@medsci.uu.se>
2022-01-12 11:34:04 +0100

    Add git merge tool

--- bin_sysman/playmiarkaansible.sh
+++ bin_sysman/playmiarkaansible.sh
@@ -2,10 +2,9 @@
 # Play an ansible playbook with local inventory file
 
 if [[ $# -eq 0 ]]; then
-	echo "Usage: play.sh <role> [options]"  
+	echo "Usage: play.sh <role, not playbook> [options]"  
 	exit 1
 fi
 
-ansible-playbook -i /home/edvard/sources/snpseq/miarka-provision/local_inventory.yml install.yml --e site=upps -t "$1" 
+ansible-playbook -i /home/edvard/sources/snpseq/miarka-provision/local_inventory.yml install.yml --e site=upps -t "$@" 
 
-echo "$@"
