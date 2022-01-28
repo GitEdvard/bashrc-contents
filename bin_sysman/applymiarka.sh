472f4ef96a0c44e6d61e1714f96a2ccb8d6eaafc
Edvard Englund <edvard.englund@medsci.uu.se>
2022-01-28 14:16:40 +0100

    Adapting to miarka

--- bin_sysman/applymiarka.sh
+++ bin_sysman/applymiarka.sh
@@ -4,9 +4,11 @@
 # Requires to be called from Miarka deployment node
 
 if [[ $# -eq 0 ]]; then
-	echo "Usage: play.sh <role, not playbook> [options]"  
+	echo "Usage: play.sh <role, not playbook> [options]"
 	exit 1
 fi
 
-ansible-playbook -i /home/edvard/sources/miarka-provision/inventory.yml install.yml --e site=upps --e deployment_remote_path=/vulpes/ngi/devel-edvard --e deployment_environment=devel deployment_version=0.1 -t "$@" 
+#singularity run --bind /home/edvard/test-deploy /vulpes/ngi/miarka-ansible.sif ansible-playbook -i /home/edvard/sources/miarka-provision/inventory.yml install.yml --e site=upps --e deployment_remote_path=/home/edvard/test-deploy --e deployment_environment=devel --e deployment_version=123 --e anaconda_path=/home/edvard/miniconda3 --e root_path=/home/edvard/test-deploy --e ngi_pipeline_venv=/home/edvard/miniconda3/envs/NGI --e ngi_pipeline_venv3=/home/edvard/miniconda3/envs/NGI_py3 -t "$@"
+
+ansible-playbook -i /home/edvard/sources/miarka-provision/inventory.yml install.yml --e site=upps --e deployment_remote_path=/home/edvard/test-deploy --e deployment_environment=devel --e anaconda_path=/home/edvard/miniconda3 --e root_path=/home/edvard/test-deploy --e ngi_pipeline_venv=/home/edvard/miniconda3/envs/NGI --e ngi_pipeline_venv3=/home/edvard/miniconda3/envs/NGI_py3 -t "$@"
 
