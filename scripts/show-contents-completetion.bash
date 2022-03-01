fb3f25ca4b0961868235543838eaae97cb0caa81
Edvard Englund <edvard.englund@medsci.uu.se>
2022-03-01 15:16:37 +0100

    Add completion to show-contents.sh

--- scripts/show-contents-completetion.bash
+++ scripts/show-contents-completetion.bash
@@ -1,7 +1,5 @@
 function _show_contents () {
-    if [[ "${#COMP_WORDS[@]}" == "2" ]]; then
-	COMPREPLY=($(compgen -W "$(git ls-files -m -o --exclude-standard)" ""))
-    fi
+    COMPREPLY=($(compgen -W "$(ls ~/bin/bin_*)" "${COMP_WORDS[1]}"))
 }
 
 complete -F _show_contents show-contents.sh
