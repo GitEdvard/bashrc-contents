7561046b98464d687c6794d780a148b5bdf532b4
Edvard Englund <edvard.englund@medsci.uu.se>
2022-03-02 16:21:00 +0100

    nuke deploy

---
+++ bin_miarka/nuke-deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+rm -rf /home/edvard/test-deploy
+mkdir /home/edvard/test-deploy
+mkdir /home/edvard/test-deploy/conf
+mkdir /home/edvard/test-deploy/containers
+mkdir /home/edvard/test-deploy/proj_root
+mkdir /home/edvard/test-deploy/resources
+mkdir /home/edvard/test-deploy/sw
+mkdir /home/edvard/test-deploy/sw/rnaseq
